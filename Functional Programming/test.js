64434c8bbb4107ed47e00b344cedb2f5445c4ecb
Shukree <shukree@shukreeabuwi.com>
2018-07-28 23:40:20 -0400

    git commit

--- Functional Programming/test.js
+++ Functional Programming/test.js
@@ -1,17 +1,25 @@
-var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+// var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
 
-/* This function should add a book to the list and return the list */
-// New parameters should come before the bookName one
+// /* This function should add a book to the list and return the list */
+// // New parameters should come before the bookName one
 
-// Add your code below this line
-function add(arr, bookName) {
-  let newList = [...arr];
-  newList.push(bookName);
-  return newList;
+// // Add your code below this line
+// function add(arr, bookName) {
+//   let newList = arr;
+//   newList.push(bookName);
+//   return newList;
   
-  // Add your code above this line
-}
+//   // Add your code above this line
+// }
 
-console.log(add(bookList, "New Book Name"));
+// console.log(add(bookList, "New Book Name"));
 
-console.log(bookList);
+// console.log(bookList);
+
+
+let myArray = [1, 2, 3, 4, 5];
+
+let newArray = myArray.map(x => x + 2);
+
+console.log(newArray);
+
