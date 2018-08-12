ab486a283c40693be16a820ac20665696baa61f8
Shukree <shukree@shukreeabuwi.com>
2018-08-12 18:42:33 -0400

    git commit

--- Intermediate Algorithm Scripting/Map-the-Debris.js
+++ Intermediate Algorithm Scripting/Map-the-Debris.js
@@ -11,15 +11,34 @@ The radius of the earth is 6367.4447 kilometers, and the GM value
 of earth is 398600.4418 km3s-2. */
 
 function orbitalPeriod(arr) {
+    var myArray = [];
     var GM = 398600.4418;
     var earthRadius = 6367.4447;
+    var a = earthRadius + 35873.5553;
 
-    var T = (2 * Math.PI) * Math.sqrt(((Math.pow(earthRadius, 3)) / GM));
-    console.log(T);
-    return arr;
+    arr.forEach(function(item, index) {
+      var obj = {
+        'name': item.name
+      }
+
+      var a = earthRadius + item.avgAlt;
+      var T = Math.round(2 * Math.PI * Math.sqrt(Math.pow(a, 3) / GM));
+      obj.orbitalPeriod = T;
+
+      myArray.push(obj);
+
+
+    });
+
+    
+   // console.log(Math.round(T));
+
+    return myArray;
   }
   
 console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}])); // should return [{name: "sputnik", orbitalPeriod: 86400}].
-orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]) // should return [{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}].
+console.log(orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}])); // should return [{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}].
+
+
 
 
